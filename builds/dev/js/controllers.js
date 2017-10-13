3314e8597a3b50bf243782944b70c00a2b34205a
stefan <stefan.emailacct@gmail.com>
2017-10-13 19:45:43 +0100

    details page

--- builds/dev/js/controllers.js
+++ builds/dev/js/controllers.js
@@ -5,77 +5,71 @@ var mainController = angular.module('mainController', []);
 
 //ListController   
 mainController.controller('ListController', [
-'$scope',
-'$http',
-'$sce',
- function($scope,$http,$sce) {
-   
-   $http.get('https://api.github.com/users?callback=cusers')
-   .then(function(cusers) {
-      $scope.response = cusers;
-      console.log($scope.response);
-   });
-   
-   
-   
-      
-      
-         
+      '$scope',
+      '$http',
+      '$sce',
+       function($scope,$http,$sce) {
+         $http.get('js/data.json').then(function(res,status) {
+            if(res.status == 200) {
+               $scope.users = res.data;
+               console.log($scope.users);
+            }
             
-            
+         }); //$http.get
+       
+       //page name
+       $scope.page = document.location.hash;
+       //index
+       $scope.idx = $scope.page.indexOf('/') + 1;
+       $scope.rem = $scope.page.substring(0,$scope.idx);
+       $scope.page = $scope.page.replace($scope.rem,'');
+       console.log($scope.page);
+   
+   
 }]); //ListController
-   
-  
-
-   
-   
-   
-   
-   
-   
-   
-
-
-
-
-
-
-
 
 
 //DetailsController   
-// mainController.controller('DetailsController', [
-//       '$scope',
-//       '$http',
-//       '$sce',
-//       '$routeParams',
-//        function($scope,$http,$sce,$routeParams) {
-//          $http.get('js/data.json').then(function(res,status) {
-//             if(res.status == 200) {
-//                $scope.users = res.data;
-//                $scope.whichUser = $routeParams.userId;
-//                //not first number
-//                   if($routeParams.userId > 0) {
-//                      $scope.prevUser = Number($routeParams.userId) - 1;
-//                   } 
-//                   else {
-//                      $scope.prevUser = $scope.users.length -1;   
-//                   }
+mainController.controller('DetailsController', [
+      '$scope',
+      '$http',
+      '$sce',
+      '$routeParams',
+       function($scope,$http,$sce,$routeParams) {
+         $http.get('js/data.json').then(function(res,status) {
+            if(res.status == 200) {
+               $scope.users = res.data;
+               $scope.whichUser = $routeParams.userId;
+               //not first number
+                  if($routeParams.userId > 0) {
+                     $scope.prevUser = Number($routeParams.userId) - 1;
+                  } 
+                  else {
+                     $scope.prevUser = $scope.users.length -1;   
+                  }
                   
-//                   //if not the last number
-//                   if($routeParams.userId < $scope.users.length -1) {
-//                      $scope.nextUser = Number($routeParams.userId) + 1;
-//                   }
-//                   else {
-//                      $scope.nextUser = 0;
-//                   }     
+                  //if not the last number
+                  if($routeParams.userId < $scope.users.length -1) {
+                     $scope.nextUser = Number($routeParams.userId) + 1;
+                  }
+                  else {
+                     $scope.nextUser = 0;
+                  }     
             
-//             }//if 200ok
+            }//if 200ok
             
-//          }); //$http.get
+         }); //$http.get
+         
+       //page name
+       $scope.page = document.location.hash;
+       //index
+       $scope.idx = $scope.page.indexOf('/') + 1;
+       $scope.rem = $scope.page.substring(0,$scope.idx);
+       $scope.page = $scope.page.replace($scope.rem,'');
+       console.log($scope.page);
    
    
-// }]); //DetailsController
+}]); //DetailsController
 
 
 
